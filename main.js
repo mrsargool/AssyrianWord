769a65a2811d6c3f414efbce5e4f68d3f62778a6
danburk1 <danbur1@nilesk12.org>
2026-01-21 23:53:07 -0600

    Allow spaces in dictionary searches

--- main.js
+++ main.js
@@ -351,7 +351,7 @@ function transliterateSelection() {
 }
 
 function defineSelection(){
-    const selection = window.getSelection().toString().replace(/\p{Mn}/gu, "").replace(/[^\u0710-\u072C]/g, ""); // Only keep assyrian letters, drop all accent marks
+    const selection = window.getSelection().toString().replace(/\p{Mn}/gu, "").replace(/[^\u0710-\u072C\s]/g, ""); // Only keep assyrian letters & spaces, drop all accent marks
     window.open('https://www.sharrukin.io/assyrian-dictionary/?search=' + encodeURIComponent(selection), '_blank');
 }
 
@@ -362,5 +362,6 @@ function closeModal(e) {
     document.getElementById('translit-modal').style.display = 'none';
 
 }
+
 
 
